ccfd43c1f2c037844114eebad75233f482e6f927
Art Lyne <Artlynee@gmail.com>
2020-03-09 20:15:01 +0500

    now starts game engine with game module

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,10 @@
 #!usr/bin/env python3
-from brain_games import brain_even
+from brain_games import engine
+from brain_games.games import even
 
 
 def main():
-    brain_even.game_even()
+    engine.game_core(even)
 
 
 if __name__ == '__main__':
