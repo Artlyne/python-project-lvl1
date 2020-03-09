1e1d19dad99c493a1fd6f880da4268bd1c0a7c08
Art Lyne <Artlynee@gmail.com>
2020-03-09 20:05:30 +0500

    init

---
+++ brain_games/games/calc.py
@@ -0,0 +1,14 @@
+import random
+import operator
+
+game_task = 'What is the result of the expression?'
+
+
+def ask():
+    math_operators = {'+': operator.add, '-': operator.sub, '*': operator.mul}
+    number1 = random.randint(1, 100)
+    number2 = random.randint(1, 100)
+    random_operator = random.choice(['+', '-', '*'])
+    question = f'{number1} {random_operator} {number2}'
+    correct_answer = math_operators[random_operator](number1, number2)
+    return question, str(correct_answer)
---
+++ brain_games/games/even.py
@@ -0,0 +1,11 @@
+import random
+
+game_task = 'Answer "yes" if number even otherwise answer "no".'
+
+
+def ask():
+    number = random.randint(1, 100)
+    question = number
+    is_number_even = (number % 2 == 0)
+    correct_answer = 'yes' if is_number_even else 'no'
+    return question, correct_answer
