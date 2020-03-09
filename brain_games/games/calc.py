02667328195adca31828062b574891a98a111110
Art Lyne <Artlynee@gmail.com>
2020-03-09 20:25:39 +0500

    changed difficulty, now max number is 10 (was 100)

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,8 +6,8 @@ game_task = 'What is the result of the expression?'
 
 def ask():
     math_operators = {'+': operator.add, '-': operator.sub, '*': operator.mul}
-    number1 = random.randint(1, 100)
-    number2 = random.randint(1, 100)
+    number1 = random.randint(1, 10)
+    number2 = random.randint(1, 10)
     random_operator = random.choice(['+', '-', '*'])
     question = f'{number1} {random_operator} {number2}'
     correct_answer = math_operators[random_operator](number1, number2)
