981b0c938480161351beccf686813994263bd8f0
Art Lyne <Artlynee@gmail.com>
2020-03-11 00:01:19 +0500

    added game logic

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,7 +1,21 @@
+import random
+
 game_task = 'Find the greatest common divisor of given numbers.'
 
 
+# Euclidean algorithm
+def gcd(a, b):
+    while a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+    return a + b
+
+
 def ask():
-    question = None
-    correct_answer = None
+    number1 = random.randint(1, 100)
+    number2 = random.randint(1, 100)
+    question = f'{number1} {number2}'
+    correct_answer = gcd(number1, number2)
     return question, correct_answer
