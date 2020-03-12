9d1d35a05875d72edc73f7361a42d82a2b62a1de
Art Lyne <Artlynee@gmail.com>
2020-03-12 22:41:43 +0500

    added logic to generate progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,4 +1,19 @@
+import random
+
 game_task = 'What number is missing in the progression?'
+
+
+def random_progression():
+    progression_start = random.randint(1, 50)
+    progression_step = random.randint(1, 10)
+    progression = [progression_start]
+    next_number = progression_start
+    i = 1
+    while i != 10:
+        next_number += progression_step
+        progression.append(next_number)
+        i += 1
+    return progression
 
 
 def ask():
