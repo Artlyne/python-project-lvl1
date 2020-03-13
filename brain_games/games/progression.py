7bd547f11db5f98118bb789dce0c0c5aa4d4c479
Art Lyne <Artlynee@gmail.com>
2020-03-13 12:33:25 +0500

    code refactoring for progression, added logic for game

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,17 +6,25 @@ game_task = 'What number is missing in the progression?'
 def random_progression():
     progression_start = random.randint(1, 50)
     progression_step = random.randint(1, 10)
-    progression = [progression_start]
-    next_number = progression_start
-    i = 1
+    progression = []
+
+    i = 0
     while i != 10:
-        next_number += progression_step
-        progression.append(next_number)
+        progression.append(progression_start)
+        progression_start += progression_step
         i += 1
+
     return progression
 
 
 def ask():
-    question = None
-    correct_answer = None
+    progression = random_progression()  # len of progression is 10
+    extractable_number = random.randint(0, 9)  # index of this number
+    correct_answer = progression[extractable_number]
+    progression[extractable_number] = '..'
+
+    question = ''
+    for i in range(len(progression)):
+        question += f'{progression[i]} '
+
     return question, correct_answer
