8c0f4e79561497b5156669911efad536e11a02d9
Art Lyne <Artlynee@gmail.com>
2020-03-13 13:23:49 +0500

    added logic for game

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,7 +1,21 @@
+import random
+
+
 game_task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
+def is_number_prime(number):
+    divisor = 2
+    while number % divisor != 0:
+        divisor += 1
+    if number == divisor:
+        return True
+    else:
+        return False
+
+
 def ask():
-    correct_answer = None
-    question = None
+    number = random.randint(2, 101)
+    question = number
+    correct_answer = 'yes' if is_number_prime(number) else 'no'
     return question, correct_answer
