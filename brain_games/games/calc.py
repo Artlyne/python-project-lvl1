6a28e9513e2c5838075c8b38bc3a92858132a07d
Art Lyne <Artlynee@gmail.com>
2020-03-18 22:05:39 +0500

    refactor: changed math_operators for better reading

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,13 +2,13 @@ import random
 import operator
 
 GAME_TASK = 'What is the result of the expression?'
+MATH_OPERATORS = ('+', operator.add), ('-', operator.sub), ('*', operator.mul)
 
 
 def generate_round():
-    math_operators = {'+': operator.add, '-': operator.sub, '*': operator.mul}
     number1 = random.randint(1, 10)
     number2 = random.randint(1, 10)
-    random_operator = random.choice(['+', '-', '*'])
-    question = f'{number1} {random_operator} {number2}'
-    correct_answer = str(math_operators[random_operator](number1, number2))
+    symbol, math_operator = random.choice(MATH_OPERATORS)
+    question = f'{number1} {symbol} {number2}'
+    correct_answer = str(math_operator(number1, number2))
     return question, correct_answer
