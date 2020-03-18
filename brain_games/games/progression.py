ef5bbf0219c7175f36837a56cf8160d52aceea33
Art Lyne <Artlynee@gmail.com>
2020-03-18 22:23:05 +0500

    refactor: question now generated with join

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,14 +3,14 @@ import random
 GAME_TASK = 'What number is missing in the progression?'
 
 
-def random_progression():
+def generate_progression():
     progression_start = random.randint(1, 50)
     progression_step = random.randint(1, 10)
     progression = []
 
     i = 0
     while i != 10:
-        progression.append(progression_start)
+        progression.append(str(progression_start))
         progression_start += progression_step
         i += 1
 
@@ -18,13 +18,10 @@ def random_progression():
 
 
 def generate_round():
-    progression = random_progression()  # len of progression is 10
+    progression = generate_progression()  # len of progression is 10
     extractable_number = random.randint(0, 9)  # index of this number
-    correct_answer = str(progression[extractable_number])
+    correct_answer = progression[extractable_number]
     progression[extractable_number] = '..'
-
-    question = ''
-    for i in range(len(progression)):
-        question += f'{progression[i]} '
+    question = ' '.join(progression)
 
     return question, correct_answer
